2c9993eba80e263846a6068bcd32d7e60a6032ff
berga <bergamot.ads1@gmail.com>
2024-07-27 05:02:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+
+
+
+
+
+
+
+ResizableArray ra = new ResizableArray();
+Console.WriteLine("Initial size: " + ra.GetSize());
+
+ra.Resize(2.0);
+Console.WriteLine("Size after resize (factor 2.0): " + ra.GetSize());
+
+
+
+
+
+
+
+
+interface IResizable
+{
+    void Resize(double factor);
+    int GetSize();
+}
+
+public class ResizableArray : IResizable
+{
+    public static int SIZE = 5;
+    private int[] numbers;
+    public ResizableArray()
+    { 
+        numbers = new int[SIZE];
+    }
+    
+
+    public void Resize(double factor)
+    {
+        int newSize = (int)(numbers.Length * factor);
+
+        int[] newNumbers = new int[newSize];
+
+        for (int i = 0; i < newNumbers.Length && i < numbers.Length; i++)
+        {
+            newNumbers[i] = numbers[i];
+        }
+
+        numbers = newNumbers;
+    }
+
+
+
+    public int GetSize()
+    {
+       return numbers.Length;
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
